420c68c29944cdd0a9390db797e23d9b41e79005
Ben Smus <bensmus@Bens-MacBook-Pro.local>
2019-04-16 21:11:03 -0700

    User applied clockwise and counterclockwise accel

--- pendulum.py
+++ pendulum.py
@@ -9,10 +9,11 @@ screen = pygame.display.set_mode((400, 400))
 clock = pygame.time.Clock()
 specklist = []
 
-def drawLine(screen, x0, y0, len, angle):
+def getXY(len, angle):
     'Coordinate system is relative to y axis'
-    tip = [x0 + len * math.sin(angle), y0 + len * math.cos(angle)]
-    pygame.draw.line(screen, BLACK, [x0, y0], tip, 10)
+    x = len * math.sin(angle)
+    y = len * math.cos(angle)
+    return x, y
 
 
 def angleAccelGrav(grav, len, angle):
@@ -28,6 +29,15 @@ def angleAccelDrag(C, w):
     return angle_accel
 
 
+def angleAccelUser(pressedPos, pressedNeg, w):
+    angle_accel = 0
+    if pressedPos:
+        angle_accel = 100
+    elif pressedNeg:
+        angle_accel = -100
+    return angle_accel
+
+
 len = 100
 angle = math.pi / 2
 w = 3
@@ -39,6 +49,7 @@ C = 0.07
 time.sleep(1)
 running = True
 while running:
+    pressedPos = pressedNeg = False
     screen.fill(WHITE)
     # at 2/3 speed
     dt = clock.tick(50) / 2500  # seconds, not mili
@@ -46,13 +57,28 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        # check for a key press
+        if event.type == pygame.KEYDOWN:
+            # the up and down arrows
+            if event.key == pygame.K_w:
+                pressedPos = True
+            elif event.key == pygame.K_s:
+                pressedNeg = True
 
-    # Update the angle
-    angle_accel = angleAccelGrav(grav, len, angle) + angleAccelDrag(C, w)
+    ### update the angle ###
+    angle_accel = angleAccelGrav(grav, len, angle) + angleAccelDrag(C, w) \
+    + angleAccelUser(pressedPos, pressedNeg, w)
+
     w += angle_accel * dt
     angle += w * dt
+    ########################
 
-    drawLine(screen, x0, y0, len, angle)
+    x, y = getXY(len, angle)
+    pygame.draw.line(screen, BLACK, [x0, y0], [x + x0, y + y0], 5)
+    pygame.draw.circle(screen, BLACK, (int(x + x0), int(y + y0)), 10)
+    pygame.draw.circle(screen, BLACK, (x0, y0), 10)
     pygame.display.update()
+    print(pressedNeg)
 
+print(angle_accel)
 pygame.quit()
